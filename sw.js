4d06ebee3d776def6d8b276c66e45d703ef4af6d
Jason <jasongu9@gmail.com>
2026-06-24 20:48:06 -0500

    v260624_7: fixed mobile loading and formating issues

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker for the Portfolio dashboard PWA.
    Bump CACHE on each release (match your YYMMDD_N version) to retire old caches. */
-const CACHE = 'ledger-260624_6';
+const CACHE = 'ledger-260624_7';
 const SHELL = [
   './',
   './index.html',
