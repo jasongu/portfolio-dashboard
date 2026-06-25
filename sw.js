adb813e08877599e6b51d585c0463d900b13a42c
Jason <jasongu9@gmail.com>
2026-06-24 20:57:40 -0500

    v260624-8: fixed overflow issues

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker for the Portfolio dashboard PWA.
    Bump CACHE on each release (match your YYMMDD_N version) to retire old caches. */
-const CACHE = 'ledger-260624_7';
+const CACHE = 'ledger-260624_8';
 const SHELL = [
   './',
   './index.html',
